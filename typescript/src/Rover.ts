99379a5ed4433704ff5e8c2376b5bf1a02c8a166
ImanolRP <imanol_ramirez@hotmail.com>
2024-03-08 19:13:27 +0100

    add test green should return 0:0:W when L

--- typescript/src/Rover.ts
+++ typescript/src/Rover.ts
@@ -2,6 +2,8 @@ const compassRight = ['N', 'E', 'S', 'W'];
 
 export default class Rover {
   execute(command: string): string {
+      if(command === 'L') return '0:0:W'
       return `0:0:${compassRight[command.length % compassRight.length]}`
   }
 }
+
--- typescript/tests/Rover.spec.ts
+++ typescript/tests/Rover.spec.ts
@@ -19,4 +19,11 @@ describe('Mars Rover', () => {
         expect(result).toBe(expectedLandingPosition)
     })
 
+    it.each([
+      ['0:0:W', 'L'],
+      ])('should return $j when the command is $j', (expectedLandingPosition: string, commands: string) => {
+      const result = rover.execute(commands)
+      expect(result).toBe(expectedLandingPosition)
+  })
+
 })
